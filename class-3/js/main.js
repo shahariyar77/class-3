559a3baac26246f9a8bfefab643ab3b984d27f8a
shahariyar77 <72545687+shahariyar77@users.noreply.github.com>
2020-11-26 16:29:08 +0600

    Add files via upload

---
+++ class-3/js/main.js
@@ -0,0 +1,12 @@
+(function ($){
+	"use strict";
+	jQuery(document).ready (function ($){
+		$(".video-play-btn").magnific popup ({
+			type:'video',
+		});
+	});
+	jQuery (window).load (function()){
+						  
+						  }
+});
+	{(jQuery)};
